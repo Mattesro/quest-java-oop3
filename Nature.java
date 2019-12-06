973fb82839205fc16ee089e5b8c59c870f120807
Rolf Mattes <rolf.mattes@telekom.de>
2019-12-06 12:23:08 +0100

    Ergebnis in Nature angehängt

--- Nature.java
+++ Nature.java
@@ -12,7 +12,6 @@ public class Nature {
         pingou.getOut();
 
         Eagle hawkeye = new Eagle("Hawkeye");
-        // TODO : uncomment the following code in order to test it
 
         hawkeye.takeOff();
         hawkeye.ascend(120);
@@ -25,3 +24,29 @@ public class Nature {
         hawkeye.land();
     }
 }
+
+/*  Ergebnis:
+	sea18@sea18:~/JavaProjekt/Aufgabe8/quest-java-oop3$ javac Swim.java 
+	sea18@sea18:~/JavaProjekt/Aufgabe8/quest-java-oop3$ javac Penguin.java 
+	sea18@sea18:~/JavaProjekt/Aufgabe8/quest-java-oop3$ javac Nature.java 
+	sea18@sea18:~/JavaProjekt/Aufgabe8/quest-java-oop3$ javac Fly.java 
+	sea18@sea18:~/JavaProjekt/Aufgabe8/quest-java-oop3$ javac Eagle.java 
+	sea18@sea18:~/JavaProjekt/Aufgabe8/quest-java-oop3$ javac Bird.java 
+	sea18@sea18:~/JavaProjekt/Aufgabe8/quest-java-oop3$ java Nature
+	Pingou dives into the water.
+	Pingou swims downward, depths : 3
+	Pingou swims upward, depths : 2
+	Pingou swims downward, depths : 6
+	Pingou swims upward, depths : 1
+	Pingou swims upward, depths : 0
+	Pingou gets out of the water.
+	Hawkeye takes off in the sky.
+	Hawkeye flies upward, altitude: 120
+	Hawkeye glides into the air.
+	Hawkeye flies upward, altitude: 150
+	Hawkeye glides into the air.
+	Hawkeye flies downward, altitude: 10
+	Hawkeye is to hight to landing.
+	Hawkeye flies downward, altitude: 1
+	Hawkeye lands on the ground.
+*/
